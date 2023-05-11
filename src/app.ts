181e9fada2d0847eed86610bdd7396de1e348da8
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-05-11 16:49:41 +0200

    Add: socket.io config

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import http from 'http';
 import cors from 'cors';
 import express from 'express';
 import path from 'path';
+import { Server } from 'socket.io';
 
 const { PORT } = process.env;
 
@@ -14,6 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 
-export const httpServer = http
-  .createServer(app)
-  .listen(PORT, () => console.info(`Server running and listening at http://localhost:${PORT}`));
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(PORT, () => console.info(`Server running and listening at http://localhost:${PORT}`));
