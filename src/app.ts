5a28da856bd9c44a4ebf499940593270d8e90ad8
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-05-15 23:46:01 +0200

    Saving data info from users connected

--- src/app.ts
+++ src/app.ts
@@ -24,19 +24,34 @@ const io = new Server(httpServer, {
   }
 });
 
+let usersAmount = 0;
+// Save an array of users with the socket.id, username and assigned color for chat?
+const users: { [key: string]: string } = {}; 
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('register', (username) => {
+    usersAmount++;
+    users[socket.id] = username;
+    console.log(`${username} connected // Total users => ${usersAmount}`);
   });
+
+  socket.on('disconnect', () => {
+    usersAmount--;
+    const username = users[socket.id];
+    console.log(`${username} disconnected // Total users => ${usersAmount}`);
+    delete users[socket.id];
+  });
+
   socket.on('chat msg', (msg: string) => {
     // This will send the event to all connected clients, including the one that initiated the event.
-    io.emit('chat msg', msg);
+    io.emit('chat msg', { user: users[socket.id], msg });
   });
+
   socket.on('new segment', (lineLength: number, lineSegment: LinesI) => {
     // This will send the event to all clients except for the one that initiated the event
     socket.broadcast.emit('new segment', lineLength, lineSegment);
   });
+
   socket.on('clear board', () => {
     socket.broadcast.emit('clear board');
   });
