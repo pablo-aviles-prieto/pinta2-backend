28dbaf3edd51c383074785960d46eba0e2eb7a8e
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2024-05-05 23:37:53 +0200

    fix(): change import casing

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,4 @@
-export type { LinesI } from './Canvas';
-export type { UsersI } from './Users';
-export type { GameStateI, RoomsI, UserI } from './Rooms';
+export type { LinesI } from './canvas';
+export type { UsersI } from './users';
+export type { GameStateI, RoomsI, UserI } from './rooms';
 export type { ContactForm } from './ContactForm';
